2e7b4d8a1b7abfa7a29dd1e3b7ddd7e056a9dd5c
asaf4me <38263049+asaf4me@users.noreply.github.com>
2018-12-19 00:05:17 +0200

    Add files via upload

--- httpClient.c
+++ httpClient.c
@@ -6,37 +6,43 @@
 #include <string.h>
 #include <unistd.h>
 
-void error_case_handler(char *color, char *msg) {
-  if (strcmp(color, "red") == 0) {
-    printf("\033[0;31m");
-    printf("%s", msg);
-    printf("\033[0m");
-  }
-}
-
-char **parsingUrl(const int argc, const char **argv) {
-  char **parser = (char **)malloc(argc * sizeof(char *));
-  if (parser != NULL) {
-    char *element;
-    int arvIndex, index;
-    for (arvIndex = 1, index = 0; arvIndex < argc && index < argc;
-         arvIndex++, index++) {
-      element = strtok((char *)argv[arvIndex], " ");
-      parser[index] = (char *)malloc(strlen(element) + 1);
-      strcpy(parser[index], element);
-      parser[index][strlen(element)] = '\0';
+void error_case_handler(char *color, char *msg)
+{
+    if (strcmp(color, "red") == 0)
+    {
+        printf("\033[0;31m");
+        printf("%s", msg);
+        printf("\033[0m");
     }
-    return parser;
-  }
-  return NULL;
 }
 
-void freeHttp(char **httpReq, int argc) {
-  if (httpReq == NULL)
-    return;
-  for (int i = 0; i < argc - 1; i++) {
-    if (httpReq[i])
-      free(httpReq[i]);
-  }
-  free(httpReq);
+char **parsingUrl(const int argc, const char **argv)
+{
+    char **parser = (char **)malloc(argc * sizeof(char *));
+    if (parser != NULL)
+    {
+        char *element;
+        int arvIndex, index;
+        for (arvIndex = 1, index = 0; arvIndex < argc && index < argc; arvIndex++, index++)
+        {
+            element = strtok((char *)argv[arvIndex], " ");
+            parser[index] = (char *)malloc(strlen(element) + 1);
+            strcpy(parser[index], element);
+            parser[index][strlen(element)] = '\0';
+        }
+        return parser;
+    }
+    return NULL;
+}
+
+void freeHttp(char **httpReq, int argc)
+{
+    if (httpReq == NULL)
+        return;
+    for (int i = 0; i < argc - 1; i++)
+    {
+        if (httpReq[i])
+            free(httpReq[i]);
+    }
+    free(httpReq);
 }
