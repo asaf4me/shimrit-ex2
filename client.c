f30c2d4164a01da26eb480a621972e7c63f105e3
asaf4me <38263049+asaf4me@users.noreply.github.com>
2018-12-26 00:56:31 +0200

    Add files via upload

--- client.c
+++ client.c
@@ -1,14 +1,14 @@
-
+#include <assert.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <netinet/tcp.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <netinet/in.h>
-#include <netdb.h>
+#include <unistd.h>
 
 typedef enum
 {
@@ -18,7 +18,7 @@ typedef enum
 
 #define ERROR -1
 #define BUFFER 50
-#define BUFFER_SIZE 4096
+#define BUFFER_SIZE 1024
 
 /*Struct to hold all the command line arguments together*/
 typedef struct request
@@ -34,12 +34,12 @@ typedef struct request
     int length;        //total length for the header
 } Request;
 
-Request *create_request() //header constructor
+Request *create_request() // header constructor
 {
     Request *request = (Request *)malloc(sizeof(Request));
     if (request == NULL)
     {
-        printf("Memory allocation error, return null");
+        perror("malloc");
         return NULL;
     }
     request->url = NULL;
@@ -54,19 +54,21 @@ Request *create_request() //header constructor
     return request;
 }
 
-void free_request(Request *request) //free heep memory allocation
+void free_request(Request *request) // free heep memory allocation
 {
     if (request->url != NULL)
         free(request->url);
     if (request->arguments != NULL)
         free(request->arguments);
+    if (request->body != NULL)
+        free(request->body);
     free(request);
 }
 
-void message(char *color, char *msg) //making evrything more beautiful :-)
+void message(char *color, char *msg) // colored printf
 {
     if (strcmp(color, "red") == 0)
-        printf("\033[0;31mUsage: %s\033[0m", msg);
+        printf("\033[0;31m%s\033[0m", msg);
     else if (strcmp(color, "green") == 0)
         printf("\033[0;32m%s\033[0m", msg);
     else if (strcmp(color, "blue") == 0)
@@ -75,79 +77,174 @@ void message(char *color, char *msg) //making evrything more beautiful :-)
 
 bool validation(char *ptr) //validation function for the parsing
 {
-    if (strstr(ptr, "http:") != NULL || strcmp(ptr, "-p") == 0 || strchr(ptr, '=') == NULL)
+    if (strchr(ptr, '=') == NULL || ptr[0] == '=' || ptr[strlen(ptr) - 1] == '=')
         return false;
     return true;
 }
 
-int parse_arguments(char **args, Request *request, int argc, int index) //parse arguments, if it failed it return ERROR[-1]
+void debug(Request *request)
 {
-    int numOfArguments = atoi(*args);
-    if (numOfArguments == 0 || strstr(*args, "http:") != NULL || strcmp(*args, "-p") == 0 || (numOfArguments + index) >= argc - 1)
+    if (request->url != NULL)
+        printf("Url is: %s\n", request->url);
+    if (request->body != NULL)
+        printf("Body is: %s\n", request->body);
+    if (request->hostName != NULL)
+        printf("Host name is: %s\n", request->hostName);
+    if (request->path != NULL)
+        printf("Path is: %s\n", request->path);
+    if (request->port != NULL)
+        printf("Port is: %s\n", request->port);
+    if (request->arguments != NULL)
     {
-        printf("Not enough arguments... return error\n");
+        printf("Arguments are: ");
+        for (int i = 0; i < request->argumentNum; i++)
+        {
+            if (request->arguments[i] != NULL)
+                printf("%s ,", request->arguments[i]);
+        }
+        printf("\n");
+    }
+}
+
+int parse_arguments(int argc, char **argv, Request *request) //parse arguments, if it failed it return ERROR[-1]
+{
+    if (request->arguments != NULL)
+    {
+        message("red", "Usage: arguments already declered\n");
+        return ERROR;
+    }
+    int index = 0, argumentsIndex = 0, counter = 0;
+    for (int i = 0; i < argc; i++)
+    {
+        if (argv[i] != NULL)
+        {
+            if (strcmp(argv[i], "-r") == 0)
+            {
+                index = i;
+                break;
+            }
+        }
+    }
+    if (index == 0)
+        return !ERROR;
+    if (index == argc - 1 && strcmp(argv[index], "-r") == 0)
+    {
+        message("red", "Usage: aruments cant be empty\n");
+        return ERROR;
+    }
+    index++;
+    int numOfArguments = atoi(argv[index]);
+    if (numOfArguments == 0)
+    {
+        message("red", "Usage: after argument flag a number of arguments must be declered\n");
         return ERROR;
     }
     request->argumentNum = numOfArguments;
-    int counter = 0;
     request->arguments = (char **)malloc(numOfArguments * sizeof(char *));
-    if (request->arguments == NULL)
+    assert(request->arguments != NULL);
+    for (int i = index; i <= index + numOfArguments; i++)
     {
-        printf("Memory allocation error, return ERROR[-1]");
-        return ERROR;
-    }
-    for (int i = 0; i < numOfArguments; i++)
-    {
-        ++args;
-        if (args != NULL)
+        if (argv[i] != NULL)
         {
-            if (validation(*args) == true)
+            if (validation(argv[i]) == true)
             {
-                request->arguments[i] = *args;
+                request->arguments[argumentsIndex] = argv[i];
+                argumentsIndex++;
                 counter++;
-                request->length += strlen(*args) + 1;
+                request->length += strlen(argv[i]) + 1;
             }
         }
-        else
+    }
+    index = index + numOfArguments + 1;
+    for (int i = index; i < argc; i++)
+    {
+        if (argv[i] != NULL && index < argc - 1)
         {
-            request->arguments[i] = NULL;
+            if (strcmp(argv[i], "-p") != 0 && strstr(argv[i], "http://") == NULL)
+                return ERROR;
         }
+        else if (validation(argv[i]) == true)
+            counter++;
     }
     if (counter != numOfArguments)
     {
-        free(request->arguments);
+        if (counter > numOfArguments)
+        {
+            message("red", "Usage: too many arguments\n");
+            return ERROR;
+        }
+        message("red", "Usage: not enough arguments\n");
         return ERROR;
     }
+
     return !ERROR;
 }
 
-int parse_body(char *body, Request *request) //parse body, if it failed it return ERROR[-1]
+int parse_body(int argc, char **argv, Request *request) //parse body, if it failed it return ERROR[-1]
 {
-
-    if (strcmp(body, "-r") == 0 || strstr(body, "http:") != NULL)
+    if (request->body != NULL)
     {
-        message("red", "invalid body argument\n");
-        printf("Example for correct input:\n./client -r <num> x=1 x=2 -p hello http://www.google.com\n");
+        message("red", "Usage: body already declered\n");
         return ERROR;
     }
-    request->body = body;
-    request->contentLength = strlen(body);
+    int index = 0;
+    for (int i = 0; i < argc; i++)
+    {
+        if (argv[i] != NULL)
+        {
+            if (strcmp(argv[i], "-p") == 0)
+            {
+                index = i;
+                break;
+            }
+        }
+    }
+    if (index == 0)
+        return !ERROR;
+    if (index == argc - 1 && strcmp(argv[index], "-p") == 0)
+    {
+        message("red", "Usage: body cant be empty\n");
+        return ERROR;
+    }
+    request->body = (char *)malloc(strlen(argv[index + 1]) * sizeof(char) + 1);
+    assert(request->body != NULL);
+    strcpy(request->body, argv[index + 1]);
+    request->body[strlen(argv[index + 1])] = '\0';
+    request->contentLength = strlen(argv[index + 1]);
     request->length += request->contentLength;
+    argv[index + 1] = NULL;
     return !ERROR;
 }
 
-int parse_url(char *url, Request *request) //parse url, if it failed it return ERROR[-1]
+int parse_url(int argc, char **argv, Request *request) //parse url, if it failed it return ERROR[-1]
 {
-    request->url = (char *)malloc(strlen(url) * sizeof(char) + 1);
-    if (request->url == NULL)
+    if (request->url != NULL)
     {
-        printf("Memory allocation error, return ERROR[-1]");
+        message("red", "Usage: url already declered\n");
         return ERROR;
     }
+    char *url = NULL;
+    for (int i = 0; i < argc; i++)
+    {
+        if (argv[i] != NULL)
+        {
+            if (strstr(argv[i], "http://") != NULL)
+            {
+                url = argv[i];
+                break;
+            }
+        }
+    }
+    if (url == NULL)
+    {
+        message("red", "Usage: url not declered\n");
+        return ERROR;
+    }
+    request->url = (char *)malloc(strlen(url) * sizeof(char) + 1);
     strcpy(request->url, url);
     request->url[strlen(url)] = '\0';
     request->length += strlen(request->url);
-    char *ptr = strchr(url, '/') + 2;
+    char *ptr = strchr(request->url, '/') + 2;
     request->hostName = ptr;
     ptr = strchr(ptr, ':');
     if (ptr != NULL)
@@ -172,15 +269,12 @@ int parse_url(char *url, Request *request) //parse url, if it failed it return E
 
 char *create_http(Request *request) //connecting the http header together, if it failed it return NULL
 {
-    if (request->url == NULL)
-    {
-        return NULL;
-    }
     char *posix = (char *)malloc(request->length * sizeof(char));
+    assert(posix != NULL);
     memset(posix, 0, request->length);
     if (posix == NULL)
     {
-        printf("Memory allocation error, return null");
+        perror("malloc");
         return NULL;
     }
     if (request->body != NULL)
@@ -222,11 +316,13 @@ char *create_http(Request *request) //connecting the http header together, if it
     return posix;
 }
 
-int make_socket(Request *request) //putting the socket up
+int make_socket(Request *request, char *posix) //putting the socket up
 {
     struct hostent *hp;
     struct sockaddr_in addr;
     int live = 1, sock;
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
     if ((hp = gethostbyname(request->hostName)) == NULL) //converting host name to ip
     {
         herror("gethostbyname");
@@ -252,91 +348,78 @@ int make_socket(Request *request) //putting the socket up
         perror("connect");
         return ERROR;
     }
-    return sock;
+    message("green", "connection established!\n");
+    int sum = 0;
+    while (true)
+    {
+        int nbytes = write(sock, posix, strlen(posix));
+        sum += nbytes;
+        if (sum == strlen(posix))
+            break;
+        if (nbytes < 0)
+        {
+            perror("write");
+            exit(1);
+        }
+    }
+    sum = 0;
+    message("blue", "message sent server response:\n");
+    while (read(sock, buffer, BUFFER_SIZE - 1) != 0)
+    {
+        fprintf(stderr, "%s", buffer);
+        bzero(buffer, BUFFER_SIZE);
+    }
+    shutdown(sock, SHUT_RDWR);
+    close(sock);
+    return !ERROR;
 }
 
 int main(int argc, char *argv[])
 {
     if (argc == 1)
     {
-        message("red", "invalid command line arguments\n");
+        message("red", "Usage: invalid command line arguments\n");
         printf("Example for correct input:\n./client -r <num> x=1 x=2 -p hello http://www.google.com\n");
         return EXIT_FAILURE;
     }
     Request *request = create_request();
-    if (request == NULL)
-        return EXIT_FAILURE;
-    for (int i = 1; i < argc; i++)
+    int iterate = parse_body(argc, argv, request);
+    if (iterate == ERROR)
     {
-        if (strstr(argv[i], "http://") != NULL) //identify the url, send it to a parse function
-        {
-            if (parse_url(argv[i], request) == ERROR)
-            {
-                message("red", "url parse failed\n");
-                free_request(request);
-                return EXIT_FAILURE;
-            }
-        }
-        if (strcmp(argv[i], "-p") == 0) //identify the body, send it to a parse function
-        {
-            if (i == argc - 1) //check what ever if the flag is at the end of the command
-            {
-                message("red", "invalid body argument\n");
-                printf("Example for correct input:\n./client -r <num> x=1 x=2 -p hello http://www.google.com\n");
-                free_request(request);
-                return EXIT_FAILURE;
-            }
-            if (parse_body(argv[i + 1], request) == ERROR)
-            {
-                message("red", "body parse failed\n");
-                free_request(request);
-                return EXIT_FAILURE;
-            }
-        }
-        if (strcmp(argv[i], "-r") == 0) //identify the arguments, send it to a parse function
-        {
-            if (i == argc - 1) //check what ever if the flag is at the end of the command
-            {
-                message("red", "invalid body argument\n");
-                printf("Example for correct input:\n./client -r <num> x=1 x=2 -p hello http://www.google.com\n");
-                free_request(request);
-                return EXIT_FAILURE;
-            }
-            if (parse_arguments(&argv[i + 1], request, argc, i) == ERROR)
-            {
-                message("red", "arguments parse failed\n");
-                free_request(request);
-                return EXIT_FAILURE;
-            }
-        }
+        message("red", "Usage: body parse failed\n");
+        free_request(request);
+        return EXIT_FAILURE;
     }
-
+    iterate = parse_arguments(argc, argv, request);
+    if (iterate == ERROR)
+    {
+        message("red", "Usage: arguments parse failed\n");
+        free_request(request);
+        return EXIT_FAILURE;
+    }
+    iterate = parse_url(argc, argv, request);
+    if (iterate == ERROR)
+    {
+        message("red", "Usage: url parse failed\n");
+        free_request(request);
+        return EXIT_FAILURE;
+    }
     char *posix = create_http(request);
     if (posix == NULL)
     {
         free_request(request);
         return EXIT_FAILURE;
     }
-    int fd;
-    char buffer[BUFFER_SIZE];
     message("green", "Parse success, message about to send:\n");
-    printf("%s\n\n", posix);
-    if ((fd = make_socket(request)) == ERROR)
+    printf("%s\n", posix);
+    iterate = make_socket(request, posix);
+    if (iterate == ERROR)
     {
+        perror("socket");
         free(posix);
         free_request(request);
         return EXIT_FAILURE;
     }
-    printf("connection established! server response: \n\n");
-    write(fd, posix, strlen(posix));
-    bzero(buffer, BUFFER_SIZE);
-    while (read(fd, buffer, BUFFER_SIZE - 1) != 0)
-    {
-        fprintf(stderr, "%s", buffer);
-        bzero(buffer, BUFFER_SIZE);
-    }
-    shutdown(fd, SHUT_RDWR);
-    close(fd);
     free(posix);
     free_request(request);
     return EXIT_SUCCESS;
